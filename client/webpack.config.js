aceef0e4f9d5a1ccfa33f38726696fa96d116922
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-02-17 10:31:36 -0300

    instalando webpack-dev-server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
 
+let plugins = [];
+
+if(process.env.NODE_ENV == 'production'){
+    plugins.push(new babiliPlugin());
+}
 module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
     },
     module: {
         rules: [
@@ -16,5 +23,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins
 }      
