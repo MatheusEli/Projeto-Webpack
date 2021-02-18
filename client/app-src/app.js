03fde4fa6ebcd79320df26fba81d36a308c52513
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-02-18 00:05:37 -0300

    css em modulos e divisão de bundles

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,9 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/js/modal.js';
+
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,28 +1,91 @@
-const path = require('path');
-const babiliPlugin = require('babili-webpack-plugin');
+const path = require("path");
+const babiliPlugin = require("babili-webpack-plugin");
+const extractTextPlugin = require("extract-text-webpack-plugin");
+const optimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 let plugins = [];
 
-if(process.env.NODE_ENV == 'production'){
-    plugins.push(new babiliPlugin());
+plugins.push(new htmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
+
+plugins.push(new extractTextPlugin("styles.css"));
+
+plugins.push(
+  new webpack.ProvidePlugin({
+    $: "jquery/dist/jquery.js",
+    jQuery: "jquery/dist/jquery.js",
+  })
+);
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}))
+
+if (process.env.NODE_ENV == "production") {
+  plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+  plugins.push(new babiliPlugin());
+  plugins.push(
+    new optimizeCSSAssetsPlugin({
+      cssProcessor: require("cssnano"),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+      },
+      canPrint: true,
+    })
+  );
 }
 module.exports = {
-    entry: './app-src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins
-}      
+  entry: {app: "./app-src/app.js", vendor: ['jquery', 'bootstrap', 'reflect-metadata']},
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: extractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader",
+        }),
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+    ],
+  },
+  plugins,
+};
